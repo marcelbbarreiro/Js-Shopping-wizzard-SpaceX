b596594a83194978cc667a3064631cacf7c4a4e7
Carla <carlacardozo990@gmail.com>
2021-10-29 17:00:54 +0100

    of grid to flex

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,8 @@
-/* Carrusel*/
-var slideIndex = 1;
-showSlides(slideIndex);
+var ImgFirst = document.querySelector(".ImgFirst");
+var minitureImg = document.querySelector(".mySlides");
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+minitureImg.addEventListener("click", change);
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+function change (e) {
+  ImgFirst.src = e.target.src ;
 }
