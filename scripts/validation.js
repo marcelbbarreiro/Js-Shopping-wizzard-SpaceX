64aa1d46fd7686115d4a5cdce8111927dace4f17
Ibai Alberdi <ibaialberdi@gmail.com>
2021-11-02 10:34:27 +0100

    validate profile working with regex

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,23 +1,34 @@
 function validateProfile() {
-  let errors;
-  let firstName = document.forms["profileForm"]["firstName"].value;
+  let errors = {
+    userName: [],
+    email: [],
+    password: [],
+    repeatPassword: [],
+  };
+  let userName = document.profileForm.userName.value;
   let email = document.profileForm.email.value;
   let password = document.profileForm.password.value;
   let repeatPassword = document.profileForm.repeatPassword.value;
-  if (firstName.length() < 8 || firstName.length() > 20) {
-    errors.name.push("Firstname should have between 8 and 20 characters.");
+  console.log(userName.length, email, password, repeatPassword);
+
+  if (userName.length < 8 || userName.length > 20) {
+    errors.userName.push("Firstname should have between 8 and 20 characters.");
   }
-  if (email.length() > 50) {
+  if (email.length > 50) {
     errors.email.push("Email address should not be longer than 50 characters");
   }
-  if (password.length() < 8 || password.length > 20) {
+  var mailFormat = "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
+  if (email.match(mailFormat)) {
+    errors.email.push("Email must be a valid email address");
+  }
+  if (password.length < 8 || password.length > 20) {
     errors.password.push("Password must be between 8 and 20 characters long.");
   }
-  if (
-    !password.match(
-      "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).+$"
-    )
-  ) {
+
+  var passwordFormat =
+    "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).+$";
+  console.log(password.match(passwordFormat));
+  if (password.match(passwordFormat) == null) {
     errors.password.push(
       "Password must contain number, uppercase, lowercase and a special character"
     );
@@ -25,6 +36,7 @@ function validateProfile() {
   if (repeatPassword != password) {
     errors.repeatPassword.push("Password must be the same");
   }
+  console.log(errors);
   return errors;
 }
 
