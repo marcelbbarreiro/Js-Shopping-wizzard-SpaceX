7b05fa6cfbd200505cc942dd38957a1dad4959af
jseo123 <jose123135@gmail.com>
2021-11-02 11:06:55 +0100

    Sections defined both in html and css

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 const mainProductbtn = document.querySelector("main-product-button");
+const product1 = document.querySelector("product1")
+const product1 = document.querySelector("product2")
+const product1 = document.querySelector("product3")
+const username = document.querySelector("username")
+const adress = document.querySelector("adress")
+const delivery = document.querySelector("delivery")
 const productBtnPress = mainProductbtn.addEventListener("click", contentChange);
 
 
