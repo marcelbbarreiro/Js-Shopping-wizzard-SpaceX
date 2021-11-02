00415f448dc3a5f5c875ff5e1d751b90d841acd1
Ibai Alberdi <ibaialberdi@gmail.com>
2021-11-02 10:41:36 +0100

    typo fixed

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,25 +9,23 @@ function validateProfile() {
   let email = document.profileForm.email.value;
   let password = document.profileForm.password.value;
   let repeatPassword = document.profileForm.repeatPassword.value;
+  const mailFormat = "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
+  const passwordFormat =
+    "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).+$";
   console.log(userName.length, email, password, repeatPassword);
 
   if (userName.length < 8 || userName.length > 20) {
-    errors.userName.push("Firstname should have between 8 and 20 characters.");
+    errors.userName.push("Username should have between 8 and 20 characters.");
   }
   if (email.length > 50) {
     errors.email.push("Email address should not be longer than 50 characters");
   }
-  var mailFormat = "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
   if (email.match(mailFormat)) {
     errors.email.push("Email must be a valid email address");
   }
   if (password.length < 8 || password.length > 20) {
     errors.password.push("Password must be between 8 and 20 characters long.");
   }
-
-  var passwordFormat =
-    "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).+$";
-  console.log(password.match(passwordFormat));
   if (password.match(passwordFormat) == null) {
     errors.password.push(
       "Password must contain number, uppercase, lowercase and a special character"
