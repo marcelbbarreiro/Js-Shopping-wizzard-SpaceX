79772cd835a8bb061a74a7d8a61cfdc202040061
jseo123 <jose123135@gmail.com>
2021-11-02 12:32:06 +0100

    javascript fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
-const mainProductbtn = document.querySelector("main-product-button");
-const product1 = document.querySelector("product1")
-const product1 = document.querySelector("product2")
-const product1 = document.querySelector("product3")
-const username = document.querySelector("username")
-const adress = document.querySelector("adress")
-const delivery = document.querySelector("delivery")
+const mainProductbtn = document.querySelector(".main-product-button");
+const product1 = document.querySelector(".product1")
+const product2 = document.querySelector(".product2")
+const product3 = document.querySelector(".product3")
+const username = document.querySelector(".username")
+const adress = document.querySelector(".adress")
+const delivery = document.querySelector(".delivery")
 const productBtnPress = mainProductbtn.addEventListener("click", contentChange);
+
+console.log(mainProductbtn)
+
+mainProductbtn.style.display = "none";
 
 
 productBtnPress;
