d471444f8d68395f0d383cdf7018ce261de44e5c
jseo123 <jose123135@gmail.com>
2021-11-02 12:38:15 +0100

    small fix to js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mainProductbtn = document.querySelector(".main-product-button");
 const product1 = document.querySelector(".product1")
 const product2 = document.querySelector(".product2")
@@ -10,21 +9,10 @@ const productBtnPress = mainProductbtn.addEventListener("click", contentChange);
 
 console.log(mainProductbtn)
 
-mainProductbtn.style.display = "none";
 
 
-=======
-import { validateProfile } from "./validation.js";
-const mainProductbtn = document.querySelector("main-product-button");
-const product1 = document.querySelector("product1");
-const product1 = document.querySelector("product2");
-const product1 = document.querySelector("product3");
-const username = document.querySelector("username");
-const adress = document.querySelector("adress");
-const delivery = document.querySelector("delivery");
-const productBtnPress = mainProductbtn.addEventListener("click", contentChange);
-
->>>>>>> a01864c7d68c91f68777f1017ae522e14ab74192
 productBtnPress;
 
-function contentChange() {}
+function contentChange() {
+  
+}
